525dd5c25cdd73be9d85784f4afdcb8c2d9d2a40
Duane Northcutt <duane.northcutt@gmail.com>
2023-10-23 11:36:29 -0700

    Checkpoint.

--- RemoteBirdSensor/src/XiaoAudioSource.h
+++ RemoteBirdSensor/src/XiaoAudioSource.h
@@ -37,6 +37,31 @@ class XiaoAudioSource : public IAudioSource {
     log_i("Stopped I2S microphone");
   };
 
+/*
+  int readBytes(void* dest, int maxBytes) override {
+    int16_t buf[maxBytes];
+    int numRead = I2S.read(buf, maxBytes);
+    log_i("N: %d", numRead);
+    if (numRead == false) {
+      log_e("Microphone read failed");
+      return 0;
+    }
+    int16_t *destSamples = (int16_t *)dest;
+    int i = 0;
+//    log_i("X%d: 0x%x, 0x%x", i, destSamples[i], buf[i]);
+//    log_i("X%d: %d, %d", i, destSamples[i], buf[i]);
+    for (i = 0; i < numRead; i++) {
+      destSamples[i] = buf[i];
+    }
+
+//    log_i("XX%d: 0x%x, 0x%x", i, destSamples[i], buf[i]);
+//    i = ((i - 1) / 2);
+//    log_i("XXX%d: 0x%x, 0x%x", i, destSamples[i], buf[i]);
+
+    return numRead;
+  }
+*/
+
   int readBytes(void* dest, int maxBytes) override {
     int numRead = I2S.read((int16_t *)dest, min(I2S.available(), maxBytes));
     if (numRead == false) {
@@ -53,7 +78,7 @@ class XiaoAudioSource : public IAudioSource {
   static const int _sdataPin = 41;
   static const int _outSdPin = -1;
   static const int _inSdPin = -1;
-  static const int _sampleRate = 16000;
+  static const int _sampleRate = 44100;
   static const int _sampleSize = 16;
-  static const int _bufSize = 512;  // default is 128 samples
+  static const int _bufSize = 1024;  // default is 128 samples
 };
