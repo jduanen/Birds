f47217815cc247564e1462d2484cd61e56b2005e
Duane Northcutt <duane.northcutt@gmail.com>
2025-10-09 15:13:21 -0700

    Added confidence thresholding and non-bird labels.

--- mqtt/mqttNonBirdFilter.py
+++ mqtt/mqttNonBirdFilter.py
@@ -7,28 +7,58 @@
 #
 ################################################################################
 
+import json
+import sys
+
 from MqttRepublisher import MqttRepublisher
+
 
 MQTT_HOST = "gpuServer1.lan"
 MQTT_PORT = 1883
-MQTT_KEEPALIVE = 60
+MQTT_KEEPALIVE = 15000
 
 MQTT_SUB_TOPIC = "birdpi/detections"
 MQTT_PUB_TOPIC = "birdpi/nonbirds"
 
+NON_BIRD_LABELS = [
+    "Dog_Dog",
+    "Engine_Engine",
+    "Environmental_Environmental",
+    "Fireworks_Fireworks",
+    "Gun_Gun",
+    "Human_Human",
+    "Noise_Noise",
+    "Siren_Siren"
+]
+
+NON_BIRD_NAMES = [names.split('_') for names in NON_BIRD_LABELS]
+NON_BIRD_SCIENTIFIC_NAMES, NON_BIRD_COMMON_NAMES = zip(*NON_BIRD_NAMES)
+
 
 class NonBirdRepublisher(MqttRepublisher):
-    def _processMsg(self, inMsg):
-        #### FIXME implement this
-        outMsg = inMsg
-        return outMsg
+    def _processPayload(self, inPayload):
+        """ Return the message payload to be republished if its names are in
+             the lists of non-bird names.
+            Return None if no message is to be published in response to the
+             received message.
+        """
+        outPayload = None
+        if (inPayload['scientific'] in NON_BIRD_SCIENTIFIC_NAMES and
+            inPayload['common'] in NON_BIRD_COMMON_NAMES):
+            if (not self.minConfidence or
+                (self.minConfidence <= inPayload['confidence'])):
+                outPayload = json.dumps(inPayload)
+        return outPayload
 
 
 def main():
     repub = NonBirdRepublisher(MQTT_SUB_TOPIC, MQTT_PUB_TOPIC, MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE)
+    repub.setMinConfidence(0.0)
+    print(f"Min Confidence: {repub.getMinConfidence()}")
     if repub.connect():
-        exit(1)
+        sys.exit(1)
     repub.run()
+    repub.disconnect()
 
 if __name__ == "__main__":
     main()
