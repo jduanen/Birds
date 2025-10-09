157af87c5662817bf58fd9c6d7d0d20089437a00
Duane Northcutt <duane.northcutt@gmail.com>
2025-10-08 18:12:53 -0700

    Refactored.

--- mqttPublisher/mqttPublisher.py
+++ mqttPublisher/mqttPublisher.py
@@ -7,74 +7,108 @@
 #  (potentially overlapping) time chunk within each recording interval.
 #
 # The format of the birdpi logs is as follows:
-#  HH:MM:SS---[server][INFO] <startOffset>;<endOffset>-('<scientificName>_<commonName>', <confidence>)
+#  [server][INFO] <startOffset>;<endOffset>-('<scientificName>_<commonName>', <confidence>)
 #
 ################################################################################
 
 import logging
 import time
 
-from systemd import journal
+import json
+import paho.mqtt.client as mqtt
 from parse import parse
+from systemd import journal
 
 
-LOG_LEVEL = "WARNING"
+LOG_LEVEL = "DEBUG"  ##"WARNING"
 
 UID = 1000          # uid of 'jdn'
 WAIT_MSEC = 15000   # currently: 15sec batches, six 3sec chunks, with .5sec overlap
+
+MQTT_HOST = "gpuServer1.lan"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE = 60
 
 
 logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
-def main():
+
+def initJournalReader(uid=UID):
     j = journal.Reader()
     j.log_level(journal.LOG_INFO)
-    j.add_match(_UID=str(UID))
+    j.add_match(_UID=str(uid))
     j.seek_tail()
     j.get_previous()
-    logger.debug("Initialized")
+    logger.debug("Initialized Journal reader")
+    return j
 
-    logger.info(f"Following INFO logs for user {UID} starting from most recent logged event")
-    while True:
-        if j.wait(WAIT_MSEC):
-            while True:
-                entry = j.get_next()
-                if not entry:
-                    break
-                if 'MESSAGE' in entry:
-                    timestamp = entry['__REALTIME_TIMESTAMP']
-                    message = entry['MESSAGE']
-                    logger.debug(f"{timestamp}: <<<{message}>>>")
-                    ret = parse("[server][INFO] {};{}-({}, {})", message)
-                    if ret:
-                        parts = list(ret)
-                        logger.debug(f"parts: {parts}")
-                        if len(parts) == 4:
-                            names = parts[2].split("_")
-                            if len(names) != 2:
-                                logger.error(f"Bad names: {parts[2]}")
-                                continue
-                            scientificName = names[0].lstrip('\'"')
-                            commonName = names[1].rstrip('\'"')
-                            msgBody = {
-                                'start': float(parts[0]),
-                                'end': float(parts[1]),
-                                'confidence': float(parts[3]),
-                                'scientific': scientificName,
-                                'common': commonName
-                            }
-                            print(f"{timestamp}: {msgBody}")
-                        else:
-                            logger.debug(f"Bad parse: {parts}")
-                    else:
-                        logger.debug("Message mismatch")
-                else:
-                    logger.debug("No MESSAGE")
-        else:
+def initMqttClient(host=MQTT_HOST, port=MQTT_PORT, keepalive=MQTT_KEEPALIVE):
+    client = mqtt.Client()
+    if client.connect(host, port, keepalive) != mqtt.MQTTErrorCode.MQTT_ERR_SUCCESS:
+        logger.error("Failed to connect to %s on port %d", host, port)
+        raise Exception
+    return client
+
+def publishDetection(client, topic, msg):
+    jsonPayload = json.dumps(msg)
+    logging.debug("On topic %s, Publish %s", topic, jsonPayload)
+    client.publish(topic, jsonPayload)
+
+def getDetectionMsg(journalReader):
+    msgBody = None
+    while not msgBody:
+        if not journalReader.wait(WAIT_MSEC):
+            logger.debug("Timed out waiting on journal entry, continuing...")
             time.sleep(0.1)
-    logger.debug("Exiting")
+            continue
+        entry = journalReader.get_next()
+        if not entry or 'MESSAGE' not in entry:
+            logger.debug("No journal entry or no MESSAGE in the entry, trying again")
+            continue
+        msgTime = entry['__REALTIME_TIMESTAMP']
+        message = entry['MESSAGE']
+        logger.debug("%s: <<<%s>>>", msgTime, message)
+        res = parse("[server][INFO] {};{}-({}, {})", message)
+        if not res:
+            logger.debug("Unable to parse message, discarding entry")
+            continue
+        parts = list(res)
+        logger.debug("parts: %s", parts)
+        if len(parts) != 4:
+            logger.debug("Bad parse of message, discarding entry")
+            continue
+        names = parts[2].split("_")
+        if len(names) != 2:
+            logger.error("Bad names: %s, discarding entry", parts[2])
+            continue
+        intervalStart = float(parts[0])
+        intervalEnd = float(parts[1])
+        confidence = float(parts[3])
+        scientificName = names[0].lstrip('\'"')
+        commonName = names[1].rstrip('\'"')
+        msgBody = {
+            'timestamp': msgTime.isoformat(),
+            'chunk': (intervalStart, intervalEnd),
+            'confidence': confidence,
+            'scientific': scientificName,
+            'common': commonName
+        }
+        break
+    return msgBody
 
+def main():
+    journalReader = initJournalReader()
+    mqttClient = initMqttClient()
+    topic = "birdpi/detections"
+
+    logger.info("Following INFO logs for user %s starting from most recent logged event", UID)
+    while True:
+        detectionMsg = getDetectionMsg(journalReader)
+        if not detectionMsg:
+            break
+        publishDetection(mqttClient, topic, detectionMsg)
+    logger.debug("Exiting")
 
 if __name__ == "__main__":
     main()
