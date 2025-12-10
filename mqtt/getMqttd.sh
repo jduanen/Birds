641843bbb1d6e35598c7caaefd22469cc29ab47e
Duane Northcutt <duane.northcutt@gmail.com>
2025-12-10 14:05:11 -0800

    Checkout just mqtt/.

--- mqtt/getMqttd.sh
+++ mqtt/getMqttd.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 #
-# Script to get the latest files for birdpi mqttd
+# Script to get the latest files for birdpi mqttd from github
 
-USER="jdn"
-SOURCE_MACHINE="gpuServer1.lan"
-SOURCE_PATH="/home/jdn/Code/Birds/mqtt/"
-TARGET_PATH="/home/jdn/Code/mqtt/"
 
-rsync -avz ${USER}@${SOURCE_MACHINE}:${SOURCE_PATH} ${TARGET_PATH}
+#wget -r -np -nH --cut-dirs=1 -l 5 https://raw.githubusercontent.com/jduanen/Birds/tree/main/mqtt/
+#wget -r -np -nH --cut-dirs=3 \
+#  -A '*.py,*.sh,*.conf,*.txt,*.md,*.service' \
+#  -R 'index.html*,*.png,*.jpg,*.css,*.js' \
+#  -l 5 \
+#  https://github.com/jduanen/Birds/tree/main/mqtt/
+
+## see available files
+#curl -s https://api.github.com/repos/jduanen/Birds/contents/mqtt/ | jq '.[].name'
+
+
+cd ${HOME}/Code
+git clone --no-checkout --filter=blob:none --sparse https://github.com/jduanen/Birds.git
+
+cd ./Code/Birds
+git sparse-checkout set mqtt/
+git checkout main
+rm ./BirdPiSensor.png ./BirdSensor.code-workspace ./LICENSE 
