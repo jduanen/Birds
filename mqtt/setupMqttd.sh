63a0c84712fdbe36f2573ca7b85dff07bc252e22
Duane Northcutt <duane.northcutt@gmail.com>
2025-12-10 14:54:18 -0800

    Created symlinks. Start service.

--- mqtt/setupMqttd.sh
+++ mqtt/setupMqttd.sh
@@ -10,3 +10,12 @@ if [ ! -d "$LOG_PATH" ]; then
     sudo chown jdn:jdn "$LOG_PATH"
     sudo chmod 750 "$LOG_PATH"
 fi
+
+MQTTD_PATH="${HOME}/Code/Birds/mqtt"
+sudo ln -s ${MQTTD_PATH}/etc/systemd/mqttd.conf /etc/systemd/
+sudo ln -s ${MQTTD_PATH}/etc/systemd/system/mqttd.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable mqttd.service
+sudo systemctl start mqttd.service
+sudo systemctl status mqttd.service
